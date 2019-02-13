b4a98f7e92ccb1b4335affd92767641124318978
Dean Wagner <wagner@nmb.gov>
2019-02-13 16:51:51 -0500

    yeahgit add -A!  working log in

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import checkersstore from '@/store/modules/checkersstore'
+import cker from './modules/checkersstore'
+import FB from './modules/firebasestore'
+import Firebase from '../firebase'
+Firebase.init();  //  initial firebase and set auth listener
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    checkersstore
+    cker, FB
   },
   state: {
     a: 'AAAA'
+  },
+  mutations: {
+
+  },
+  actions: {
+    googlelogin (context) {
+      console.log("in the store action googlelogin")
+      Firebase.googlelogin()
+    },
+    logout (context) {
+      console.log("in the store action logout")
+      Firebase.logout()
+    },
+    onAuth (context) {
+      console.log("in the store action onAuth")
+      Firebase.onAuth()
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <Authentication />
     <p>{{testdata}}</p>
   </div>
 </template>
 
 <script>
+import Authentication from '@/components/Authentication'
+
   export default {
+    components: {
+      Authentication
+    },
     computed: {
       testdata: function () {
         return this.$store.state.testdata;
