5a8a566052bcf7e8d1f2211204e53060cf81580f
Dean Wagner <wagner@nmb.gov>
2019-02-27 12:25:53 -0500

    loading matches

--- src/store/modules/checkersstore.js
+++ src/store/modules/checkersstore.js
@@ -1,12 +1,34 @@
 
 export default {
   state: {
-    match: 'rrrrrrrrrrrreeeeeeeebbbbbbbbbbbb'
+    match: {gameLog: [ 'a','b','c','d'], red: 'dean', blk: 'logan', status: 'active'},
+    matchid: null
+  },
+  getters: {
+    match(state) {
+      return state.match;
+    },
+    matchid(state) {
+      return state.matchid;
+    },
+    currentBoardState(state) {
+      let glog = state.match.gameLog
+      let lastIndex = state.match.gameLog.length - 1
+      return state.match.gameLog[lastIndex];
+    }
   },
   mutations: {
-
+    setMatchId(state, matchid) {
+      state.matchid = matchid; 
+    },
+    setMatch(state, match) {
+      state.match = match; 
+    },
   },
   actions: {
-
+    matchIdChange ( context, payload ) {
+      context.commit('setMatchId', payload);
+      console.log("just dumped the match id " + payload)
+    }
   }
 }
--- src/views/Checkers.vue
+++ src/views/Checkers.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
+    <v-text-field
+                  v-model="matchid"
+                  label="Match ID"
+                  @change="matchChange"
+                >
+    </v-text-field>
+    <p>{{currentBoardState}}</p>
     <h1>This is a checkboard</h1>
     <checkerboard />
   </div>
@@ -13,6 +20,21 @@ export default {
   components: {
       Checkerboard
   },
+  data () {
+    return {
+      matchid: "yo"
+    }
+  },
+  computed: {
+    currentBoardState() {
+      return this.$store.getters.currentBoardState;
+    },
+  },
+  methods: {
+    matchChange () {
+      this.$store.dispatch('matchIdChange', this.matchid);
+    }
+  }
   
 }
 </script>
