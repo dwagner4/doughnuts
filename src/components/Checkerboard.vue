ca78fb6cfcd1be3bfbca7674f8c467f90ed5463d
Dean <dean@dfwagners.net>
2019-03-31 07:40:54 -0700

    new checkerboard, using floating div pieces

--- src/components/Checkerboard.vue
+++ src/components/Checkerboard.vue
@@ -1,69 +1,95 @@
 <template>
   <div id="board">
-    <div class="white row0 col0"><img :src="img0" /></div>
+    <div class="white row0 col0"></div>
     <div class="grey row0 col1"></div>
-    <div class="white row0 col2"><img :src="img2" /></div>
+    <div class="white row0 col2"></div>
     <div class="grey row0 col3"></div>
-    <div class="white row0 col4"><img :src="img3" /></div>
+    <div class="white row0 col4"></div>
     <div class="grey row0 col5"></div>
-    <div class="white row0 col6"><img :src="img4" /></div>
+    <div class="white row0 col6"></div>
     <div class="grey row0 col7"></div>
     <div class="grey row1 col0"></div>
-    <div class="white row1 col1"><img :src="img5" /></div>
+    <div class="white row1 col1"></div>
     <div class="grey row1 col2"></div>
-    <div class="white row1 col3"><img :src="img6" /></div>
+    <div class="white row1 col3"></div>
     <div class="grey row1 col4"></div>
-    <div class="white row1 col5"><img :src="img7" /></div>
+    <div class="white row1 col5"></div>
     <div class="grey row1 col6"></div>
-    <div class="white row1 col7"><img :src="img8" /></div>
-    <div class="white row2 col0"><img :src="img9" /></div>
+    <div class="white row1 col7"></div>
+    <div class="white row2 col0"></div>
     <div class="grey row2 col1"></div>
-    <div class="white row2 col2"><img :src="img10" /></div>
+    <div class="white row2 col2"></div>
     <div class="grey row2 col3"></div>
-    <div class="white row2 col4"><img :src="img11" /></div>
+    <div class="white row2 col4"></div>
     <div class="grey row2 col5"></div>
-    <div class="white row2 col6"><img :src="img12" /></div>
+    <div class="white row2 col6"></div>
     <div class="grey row2 col7"></div>
     <div class="grey row3 col0"></div>
-    <div class="white row3 col1"><img :src="img13" /></div>
+    <div class="white row3 col1"></div>
     <div class="grey row3 col2"></div>
-    <div class="white row3 col3"><img :src="img14" /></div>
+    <div class="white row3 col3"></div>
     <div class="grey row3 col4"></div>
-    <div class="white row3 col5"><img :src="img15" /></div>
+    <div class="white row3 col5"></div>
     <div class="grey row3 col6"></div>
-    <div class="white row3 col7"><img :src="img16" /></div>
-    <div class="white row4 col0"><img :src="img17" /></div>
+    <div class="white row3 col7"></div>
+    <div class="white row4 col0"></div>
     <div class="grey row4 col1"></div>
-    <div class="white row4 col2"><img :src="img18" /></div>
+    <div class="white row4 col2"></div>
     <div class="grey row4 col3"></div>
-    <div class="white row4 col4"><img :src="img19" /></div>
+    <div class="white row4 col4"></div>
     <div class="grey row4 col5"></div>
-    <div class="white row4 col6"><img :src="img20" /></div>
+    <div class="white row4 col6"></div>
     <div class="grey row4 col7"></div>
     <div class="grey row5 col0"></div>
-    <div class="white row5 col1"><img :src="img20" /></div>
+    <div class="white row5 col1"></div>
     <div class="grey row5 col2"></div>
-    <div class="white row5 col3"><img :src="img21" /></div>
+    <div class="white row5 col3"></div>
     <div class="grey row5 col4"></div>
-    <div class="white row5 col5"><img :src="img22" /></div>
+    <div class="white row5 col5"></div>
     <div class="grey row5 col6"></div>
-    <div class="white row5 col7"><img :src="img23" /></div>
-    <div class="white row6 col0"><img :src="img24" /></div>
+    <div class="white row5 col7"></div>
+    <div class="white row6 col0"></div>
     <div class="grey row6 col1"></div>
-    <div class="white row6 col2"><img :src="img25" /></div>
+    <div class="white row6 col2"></div>
     <div class="grey row6 col3"></div>
-    <div class="white row6 col4"><img :src="img26" /></div>
+    <div class="white row6 col4"></div>
     <div class="grey row6 col5"></div>
-    <div class="white row6 col6"><img :src="img27" /></div>
+    <div class="white row6 col6"></div>
     <div class="grey row6 col7"></div>
     <div class="grey row7 col0"></div>
-    <div class="white row7 col1"><img :src="img28" /></div>
+    <div class="white row7 col1"></div>
     <div class="grey row7 col2"></div>
-    <div class="white row7 col3"><img :src="img29" /></div>
+    <div class="white row7 col3"></div>
     <div class="grey row7 col4"></div>
-    <div class="white row7 col5"><img :src="img30" /></div>
+    <div class="white row7 col5"></div>
     <div class="grey row7 col6"></div>
-    <div class="white row7 col7"><img :src="img31" /></div>
+    <div class="white row7 col7"></div>
+    
+    <div id="r1" v-bind:class="r1class"><img v-bind:src="r1img" alt=""></div>
+    <div id="r2" v-bind:class="r2class"><img v-bind:src="r2img" alt=""></div>
+    <div id="r3" v-bind:class="r3class"><img v-bind:src="r3img" alt=""></div>
+    <div id="r4" v-bind:class="r4class"><img v-bind:src="r4img" alt=""></div>
+    <div id="r5" v-bind:class="r5class"><img v-bind:src="r5img" alt=""></div>
+    <div id="r6" v-bind:class="r6class"><img v-bind:src="r6img" alt=""></div>
+    <div id="r7" v-bind:class="r7class"><img v-bind:src="r7img" alt=""></div>
+    <div id="r8" v-bind:class="r8class"><img v-bind:src="r8img" alt=""></div>
+    <div id="r9" v-bind:class="r9class"><img v-bind:src="r9img" alt=""></div>
+    <div id="r10" v-bind:class="r10class"><img v-bind:src="r10img" alt=""></div>
+    <div id="r11" v-bind:class="r11class"><img v-bind:src="r11img" alt=""></div>
+    <div id="r12" v-bind:class="r12class"><img v-bind:src="r12img" alt=""></div>
+
+    <div id="b1" v-bind:class="b1class"><img v-bind:src="b1img" alt=""></div>
+    <div id="b2" v-bind:class="b2class"><img v-bind:src="b2img" alt=""></div>
+    <div id="b3" v-bind:class="b3class"><img v-bind:src="b3img" alt=""></div>
+    <div id="b4" v-bind:class="b4class"><img v-bind:src="b4img" alt=""></div>
+    <div id="b5" v-bind:class="b5class"><img v-bind:src="b5img" alt=""></div>
+    <div id="b6" v-bind:class="b6class"><img v-bind:src="b6img" alt=""></div>
+    <div id="b7" v-bind:class="b7class"><img v-bind:src="b7img" alt=""></div>
+    <div id="b8" v-bind:class="b8class"><img v-bind:src="b8img" alt=""></div>
+    <div id="b9" v-bind:class="b9class"><img v-bind:src="b9img" alt=""></div>
+    <div id="b10" v-bind:class="b10class"><img v-bind:src="b10img" alt=""></div>
+    <div id="b11" v-bind:class="b11class"><img v-bind:src="b11img" alt=""></div>
+    <div id="b12" v-bind:class="b12class"><img v-bind:src="b12img" alt=""></div>
   </div>
 </template>
 
@@ -74,10 +100,56 @@ import { mapGetters } from 'vuex'
 export default {
   data () {
     return {
-      img0: '', img1: '', img2: '', img3: '', img4: '', img5: '', img6: '', img7: '', 
-      img8: '', img9: '', img10: '', img11: '', img12: '', img13: '', img14: '', img15: '', 
-      img16: '', img17: '', img18: '', img19: '', img20: '', img21: '', img22: '', img23: '', 
-      img24: '', img25: '', img26: '', img27: '', img28: '', img29: '', img30: '', img31: '', 
+     
+      r1class: "row0 col0",
+      r1img: "assets/red.png",
+      r2class: "row0 col2",
+      r2img: "assets/red.png",
+      r3class: "row0 col4",
+      r3img: "assets/red.png",
+      r4class: "row0 col6",
+      r4img: "assets/red.png",
+      r5class: "row1 col1",
+      r5img: "assets/red.png",
+      r6class: "row1 col3",
+      r6img: "assets/red.png",
+      r7class: "row1 col5",
+      r7img: "assets/red.png",
+      r8class: "row1 col7",
+      r8img: "assets/red.png",
+      r9class: "row2 col0",
+      r9img: "assets/red.png",
+      r10class: "row2 col2",
+      r10img: "assets/red.png",
+      r11class: "row2 col4",
+      r11img: "assets/red.png",
+      r12class: "row2 col6",
+      r12img: "assets/red.png",
+
+      b1class: "row5 col1",
+      b1img: "assets/blk.png",
+      b2class: "row5 col3",
+      b2img: "assets/blk.png",
+      b3class: "row5 col5",
+      b3img: "assets/blk.png",
+      b4class: "row5 col7",
+      b4img: "assets/blk.png",
+      b5class: "row6 col0",
+      b5img: "assets/blk.png",
+      b6class: "row6 col2",
+      b6img: "assets/blk.png",
+      b7class: "row6 col4",
+      b7img: "assets/blk.png",
+      b8class: "row6 col6",
+      b8img: "assets/blk.png",
+      b9class: "row7 col1",
+      b9img: "assets/blk.png",
+      b10class: "row7 col3",
+      b10img: "assets/blk.png",
+      b11class: "row7 col5",
+      b11img: "assets/blk.png",
+      b12class: "row7 col7",
+      b12img: "assets/blk.png",
     }
   },
   computed: {
@@ -85,64 +157,9 @@ export default {
       'boardState', 'allMvs', 'selected', 'spacesize'
     ])
   },
-  watch: {
-    boardState: function (newBd, oldBd) {
-      let doChoice = function(code) {
-        let result = null
-        if ( code === 'r' ) {
-          result = './assets/red.png'
-        }
-        if ( code === 'b' ) {
-          result = './assets/blk.png'
-        }
-        return result
-      }
-       
-      this.img0 = doChoice(this.boardState[0])
-      this.img1 = doChoice(this.boardState[1])
-      this.img2 = doChoice(this.boardState[2])
-      this.img3 = doChoice(this.boardState[3])
-      this.img4 = doChoice(this.boardState[4])
-      this.img5 = doChoice(this.boardState[5])
-      this.img6 = doChoice(this.boardState[6])
-      this.img7 = doChoice(this.boardState[7])
-      this.img8 = doChoice(this.boardState[8])
-      this.img9 = doChoice(this.boardState[9])
-      this.img10 = doChoice(this.boardState[10])
-      this.img11 = doChoice(this.boardState[11])
-      this.img12 = doChoice(this.boardState[12])
-      this.img13 = doChoice(this.boardState[13])
-      this.img14 = doChoice(this.boardState[14])
-      this.img15 = doChoice(this.boardState[15])
-      this.img16 = doChoice(this.boardState[16])
-      this.img17 = doChoice(this.boardState[17])
-      this.img18 = doChoice(this.boardState[18])
-      this.img19 = doChoice(this.boardState[19])
-      this.img20 = doChoice(this.boardState[20])
-      this.img21 = doChoice(this.boardState[21])
-      this.img22 = doChoice(this.boardState[22])
-      this.img23 = doChoice(this.boardState[23])
-      this.img24 = doChoice(this.boardState[24])
-      this.img25 = doChoice(this.boardState[25])
-      this.img26 = doChoice(this.boardState[26])
-      this.img27 = doChoice(this.boardState[27])
-      this.img28 = doChoice(this.boardState[28])
-      this.img29 = doChoice(this.boardState[29])
-      this.img30 = doChoice(this.boardState[30])
-      this.img31 = doChoice(this.boardState[31])
-
-
-    }
-  },
+ 
   methods: {
-    spaceImg(spaceInt) {
-      let spaceState = 3
-      if ( spaceInt == 2 ) {
-        return './assets/blk.png'
-      } else {
-        return './assets/red.png'
-      }
-    }
+    movePiece( fm, to ) {},
   },
   created: function() {
 
@@ -180,9 +197,5 @@ export default {
     background-color: #555555;
     height: 63px;
   }
-  
-  .imgsz {
-    max-width: 61px;
-    max-height: 61px;
-  }
+
 </style>
