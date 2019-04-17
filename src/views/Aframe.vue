b4974466d2af04750cebfcb4ec8bfd43cc4f1771
Dean Wagner <wagner@nmb.gov>
2019-04-17 09:33:13 -0400

    3d checker board

--- src/views/Aframe.vue
+++ src/views/Aframe.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
      <a-scene>
-       <a-assets>
-        <a-asset-item id="checkers-obj" src="assets/checkers.obj"></a-asset-item>
-        <a-asset-item id="checkers-mtl" src="assets/checkers.mtl"></a-asset-item>
-      </a-assets>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-      <a-obj-model src="#checkers-obj" mtl="#checkers-mtl"></a-obj-model>
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+      <a-cylinder position="3.5 0.125 -0.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="1.5 0.125 -0.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-0.5 0.125 -0.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-2.5 0.125 -0.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="2.5 0.125 -1.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="0.5 0.125 -1.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-1.5 0.125 -1.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-3.5 0.125 -1.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="3.5 0.125 -2.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="1.5 0.125 -2.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-0.5 0.125 -2.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+      <a-cylinder position="-2.5 0.125 -2.5" radius="0.5" height="0.25" color="#dd2222"></a-cylinder>
+
+      <a-cylinder position="2.5 0.125 -7.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="0.5 0.125 -7.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-1.5 0.125 -7.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-3.5 0.125 -7.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+
+      <a-cylinder position="3.5 0.125 -6.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="1.5 0.125 -6.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-0.5 0.125 -6.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-2.5 0.125 -6.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+
+      <a-cylinder position="2.5 0.125 -5.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="0.5 0.125 -5.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-1.5 0.125 -5.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-cylinder position="-3.5 0.125 -5.5" radius="0.5" height="0.25" color="#222222"></a-cylinder>
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="8" height="8" color="#7BC8A4"></a-plane>
       <a-sky color="#ECECEC"></a-sky>
     </a-scene>
   </div>
